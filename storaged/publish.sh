ef4e474b32f6eca44e006bc59013a4fd922709ac
Tino Rusch <tino.rusch@webvariants.de>
2017-01-11 13:20:03 +0100

    fixed docker push call;

--- storaged/publish.sh
+++ storaged/publish.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 docker tag trusch/storaged:latest trusch/storaged:$(git describe)
-docker push trusch/storaged:latest trusch/storaged:$(git describe)
+docker push trusch/storaged:latest
+docker push trusch/storaged:$(git describe)
 
 exit $?
